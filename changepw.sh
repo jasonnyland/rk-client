9843dea24175dcc9a359fed9ff1ec1830cd1c43e
Jason Nyland <jason@jasonnyland.com>
2020-07-27 21:12:42 -0700

    merge firstrun into server-side setup script

--- changepw.sh
+++ changepw.sh
@@ -4,18 +4,16 @@ pass=WEBDAV_PASSWORD
 b1=Lrsp8fEfIuGFxWR2s4vj
 b2=l58vPPTyeUWe1sxTe3ZN
 
-if [ $# == 2 ]; then
-	# set new username/password
-	sed -i "s/\($name *= *\).*/\1$1/" docker-compose.yml
-	sed -i "s/\($pass *= *\).*/\1$2/" docker-compose.yml
-	# remove old dockers and re-run compose
-	sudo docker stop $(sudo docker ps -a -q)
-	sudo docker rm $(sudo docker ps -a -q)
-	sudo docker-compose up -d
-	# remove plaintext user data from dockerfile
-	sed -i "s/\($name *= *\).*/\1$b1/" docker-compose.yml
-	sed -i "s/\($pass *= *\).*/\1$b2/" docker-compose.yml
-else
-	echo "Incorrect args.  Use \"./changepw.sh user pass\""
-fi
-	
+### arg enforcement
+if [ $# -ne 2 ]; then; echo "Invalid args.  Use ./changepw.sh username password"; exit 1; fi
+# set new username/password
+sed -i "s/\($name *= *\).*/\1$1/" docker-compose.yml
+sed -i "s/\($pass *= *\).*/\1$2/" docker-compose.yml
+# remove old dockers and re-run compose
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker-compose up -d
+# remove plaintext user data from dockerfile
+sed -i "s/\($name *= *\).*/\1$b1/" docker-compose.yml
+sed -i "s/\($pass *= *\).*/\1$b2/" docker-compose.yml
+
