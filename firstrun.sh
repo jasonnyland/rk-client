a48cdab18da52a5fe5f42bd165ee87802c54d278
Jason Nyland <jason@jasonnyland.com>
2020-07-27 19:39:24 -0700

    firstrun now auto-edits nginx default file

--- firstrun.sh
+++ firstrun.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 url=URL
+srv=server_name
 name=WEBDAV_USERNAME
 pass=WEBDAV_PASSWORD
-b1=Lrsp8fEfIuGFxWR2s4vj
-b2=l58vPPTyeUWe1sxTe3ZN
+b2=Lrsp8fEfIuGFxWR2s4vj
+b3=l58vPPTyeUWe1sxTe3ZN
 
 if [ $# == 3 ]; then
     #set url
 	sed -i "s/\($url *= *\).*/\1$1/" docker-compose.yml
+	sed -i "s/\($srv *= *\).*/\1$1/" default
 	# set new username/password
-	sed -i "s/\($name *= *\).*/\1$1/" docker-compose.yml
-	sed -i "s/\($pass *= *\).*/\1$2/" docker-compose.yml
+	sed -i "s/\($name *= *\).*/\1$2/" docker-compose.yml
+	sed -i "s/\($pass *= *\).*/\1$3/" docker-compose.yml
 	# run compose
 	sudo docker-compose up -d
 	# remove plaintext user data from dockerfile
-	sed -i "s/\($name *= *\).*/\1$b1/" docker-compose.yml
-	sed -i "s/\($pass *= *\).*/\1$b2/" docker-compose.yml
+	sed -i "s/\($name *= *\).*/\1$b2/" docker-compose.yml
+	sed -i "s/\($pass *= *\).*/\1$b3/" docker-compose.yml
 else
 	echo "Incorrect args.  Use \"./firstrun.sh url user pass\""
 fi
