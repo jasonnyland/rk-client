501dc6bb40e5ec1934d824670d1a32220a1cc872
Jason Nyland <jason@jasonnyland.com>
2020-07-27 18:51:04 -0700

    merged scripts

--- firstrun.sh
+++ firstrun.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+url=URL
 name=WEBDAV_USERNAME
 pass=WEBDAV_PASSWORD
 b1=Lrsp8fEfIuGFxWR2s4vj
 b2=l58vPPTyeUWe1sxTe3ZN
 
-if [ $# == 2 ]; then
+if [ $# == 3 ]; then
+    #set url
+	sed -i "s/\($url *= *\).*/\1$1/" docker-compose.yml
 	# set new username/password
 	sed -i "s/\($name *= *\).*/\1$1/" docker-compose.yml
 	sed -i "s/\($pass *= *\).*/\1$2/" docker-compose.yml
-	# remove old dockers and re-run compose
-	sudo docker stop $(sudo docker ps -a -q)
-	sudo docker rm $(sudo docker ps -a -q)
+	# run compose
 	sudo docker-compose up -d
 	# remove plaintext user data from dockerfile
 	sed -i "s/\($name *= *\).*/\1$b1/" docker-compose.yml
 	sed -i "s/\($pass *= *\).*/\1$b2/" docker-compose.yml
 else
-	echo "Incorrect args.  Use \"./changepw.sh user pass\""
+	echo "Incorrect args.  Use \"./firstrun.sh url user pass\""
 fi
 	
